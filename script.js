4cdbd4d3fb14aa6cec212a8eeac9d4f15c3b0caa
jule.ahrens <jule.ahrens@accenture.com>
2026-09-16 21:42:59 +0200

    Rechtsschreibung

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-let zahl1 = 5;
-let zahl2 = 10;
+// let zahl1 = 5;
+// let zahl2 = 10;
 
-console.log(zahl1+zahl2);
-console.log(zahl1-zahl2);
+// console.log(zahl1+zahl2);
+// console.log(zahl1-zahl2);
+
+// console.log(zahl1++);
+// zahl1++;
+
+// console.log(5 == "5");
+// console.log(5 < 9);
+
+
+function functionName(zahl1,zahl2) {
+    let result = zahl1 + zahl2;
+    return result;
+}
+
+console.log(functionName(5, 5));
+
+let zahl1 = 5
+let zahl2 = 12
 
 
